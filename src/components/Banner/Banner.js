67f3866b02341200539bbf1508d5e17cc84c14e6
andradebru <brunansandrade@gmail.com>
2023-07-14 14:26:26 -0300

    add banner and excludes some default code

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,11 @@
+
+import './Banner.css'
+function Banner() {
+    return (
+        <header className="banner"> 
+            <img src="/imagens/banner.png" alt="hero image azul" />
+        </header>
+    )
+}
+
+export default Banner
