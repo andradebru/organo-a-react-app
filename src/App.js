f1f670d20081880d39b515361f471f0fc207da61
andradebru <brunansandrade@gmail.com>
2023-07-17 10:35:12 -0300

    add props to form

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import Banner from './components/Banner';
-import TextField from './components/TextField';
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
       <Banner />
-      <TextField label="Nome" placeholder="Digite seu nome"/>
-      <TextField label="Cargo" placeholder="Digite seu cargo"/>
-      <TextField label="Imagem" placeholder="Digite o endereço da imagem"/>
+      <Form />
     </div>
   );
 }
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,1 +1,17 @@
 import './Form.css'
+import TextField from '../TextField'
+
+const Form = () => {
+    return (
+        <section className='form'>
+            <form>
+                <h2>Preencha os dados para criar o card do colaborador</h2>
+                <TextField label="Nome" placeholder="Digite seu nome"/>
+                <TextField label="Cargo" placeholder="Digite seu cargo"/>
+                <TextField label="Imagem" placeholder="Digite o endereço da imagem"/>
+            </form>
+        </section>
+    )
+}
+
+export default Form
