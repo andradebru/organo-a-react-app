9fbb7352beb7df352b9971bdc3a191ce01825c07
andradebru <brunansandrade@gmail.com>
2023-07-18 17:12:56 -0300

    reinitialize team field after add a person

--- src/components/DropdownList/index.js
+++ src/components/DropdownList/index.js
@@ -6,6 +6,7 @@ const DropdownList = (props) => {
         <div className='dropdown'>
             <label>{props.label}</label>
                 <select onChange={evento => props.aoAlterado(evento.target.value)} required={props.required} value={props.valor}>
+                    <option value=''></option>
                     {props.itens.map(item => <option key={item}>{item}</option>)}
                 </select>
         </div>
